d9a1bdddcaa8d03a2fbf3ec6fee8f49f88fff141
Elder <elder.campbaltazar@gmail.com>
2017-04-06 17:53:49 -0700

    first commit

---
+++ textInputFiledPOC/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  textInputFiledPOC
+//
+//  Created by um tera on 4/6/17.
+//  Copyright © 2017 um tera. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var textName: kTextFiledPlaceHolder!
+
+    @IBOutlet weak var textPass: kTextFiledPlaceHolder!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        textName.directionMaterial = placeholderDirection.placeholderUp
+//        textName.difference = 35.0
+//        textName.textFieldDidChange()
+//
+//        textName.directionMaterial = placeholderDirection.placeholderUp
+//        textName.difference = 35.0
+//        textName.textFieldDidChange()
+
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
